490694b87e303a8e58043853e979840acd330c51
R0rt1z2 <rogerortizleal@gmail.com>
2021-10-22 16:10:25 +0200

    realme-ota: set default {type,choices} of rui_version
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>

--- realme_ota/main.py
+++ realme_ota/main.py
@@ -25,7 +25,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("product_model", type=str, help="Product Model (ro.product.name).")
     parser.add_argument("ota_version", help="OTA Version (ro.build.version.ota).")
-    parser.add_argument("rui_version", help="RealmeUI Version (1, 2).")
+    parser.add_argument("rui_version", type=int, choices=[1, 2], help="RealmeUI Version (ro.build.version.realmeui).")
     parser.add_argument("-c", "--server", type=int, default=0, help="Use specific server for the request (GL = 0, CN = 1, IN = 2, EU = 3).")
     parser.add_argument("-t", "--timeout", type=int, help="Use custom timeout for the request.")
     parser.add_argument("-d", "--dump", type=str, help="Save request response into file.")
@@ -51,31 +51,31 @@ def main():
     if PRODUCT != OTA_VERSION[:7]:
         PRODUCT = OTA_VERSION[:7]
     
-    if args.rui_version == "1":
+    if args.rui_version == 1:
         URL = config.RUI1_ENDPS["GL_URL"]
-    elif args.rui_version == "2":
+    elif args.rui_version == 2:
         URL = config.RUI2_ENDPS["GL_URL"]
 
     if args.server == 1:
-        if args.rui_version == "1":
+        if args.rui_version == 1:
             URL = config.RUI1_ENDPS["CN_URL"]
-        elif args.rui_version == "1":
+        elif args.rui_version == 1:
             URL = config.RUI2_ENDPS["CN_URL"]
     elif args.server == 2:
-        if args.rui_version == "1":
+        if args.rui_version == 1:
             URL = config.RUI1_ENDPS["IN_URL"]
-        elif args.rui_version == "2":
+        elif args.rui_version == 2:
             URL = config.RUI2_ENDPS["IN_URL"]
     elif args.server == 3:
-        if args.rui_version == "1":
+        if args.rui_version == 1:
             URL = config.RUI1_ENDPS["EU_URL"]
-        elif args.rui_version == "2":
+        elif args.rui_version == 2:
             URL = config.RUI2_ENDPS["EU_URL"]  
   
     if args.timeout:
         TIMEOUT = args.timeout
     
-    if args.rui_version == "1":
+    if args.rui_version == 1:
         HEADERS = config.RUI1_HEADERS
         DATA = config.RUI1_DATA
         
@@ -85,7 +85,7 @@ def main():
         DATA['time'] = TIME
         DATA['otaPrefix'] = MINOR_VERSION
 
-    elif args.rui_version == "2":
+    elif args.rui_version == 2:
         HEADERS = config.RUI2_HEADERS
         DATA = config.RUI2_DATA
                 
@@ -96,14 +96,14 @@ def main():
     if not args.silent:
         logger.log(f"RealmeUI V{args.rui_version} {PRODUCT} ({PRODUCT_IDENTIFIER}) - {MAJOR_VERSION}")
 
-    if args.rui_version == "1":
+    if args.rui_version == 1:
         try:
             response = requests.post(URL, 
                 data = json.dumps({'params': crypto.encrypt_ecb(json.dumps(DATA))}), headers = HEADERS, timeout = TIMEOUT)
         except Exception as e:
             die(f"This shouldn't happen. Something went wrong while requesting to the endpoint ({e})!", -1, 3)
         
-    elif args.rui_version == "2":
+    elif args.rui_version == 2:
         try:
             response = requests.post(URL, json = {'params': crypto.encrypt_ctr(json.dumps(DATA)).decode("utf-8")}, headers = HEADERS, timeout = 30)
         except Exception as e:
@@ -112,13 +112,13 @@ def main():
     if response.status_code != 200:
         die(f"Received invalid response: {response.status_code} :(!", -1, 2)
 
-    if args.rui_version == "1":
+    if args.rui_version == 1:
         try:
             content = json.loads(crypto.decrypt_ecb(json.loads(response.content)['resps']))
         except Exception as e:
             die(f"This shouldn't happen. Something went wrong while trying to decrypt the response ({e})!", -1, 3)
         
-    elif args.rui_version == "2":
+    elif args.rui_version == 2:
         try:
             content = json.loads(crypto.decrypt_ctr(json.loads(response.content)['body']))
         except Exception as e:
