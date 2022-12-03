1500eabf6692f83b9959f803f0453906471f3980
R0rt1z2 <rogerortizleal@gmail.com>
2022-12-03 23:58:14 +0200

    realme-ota: Add copyright header(s)
    * I'm going to EOL the tool soon anyway.

--- realme_ota/main.py
+++ realme_ota/main.py
@@ -1,3 +1,19 @@
+#
+# This file is part of realme-ota (https://github.com/R0rt1z2/realme-ota).
+# Copyright (c) 2022 Roger Ortiz.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 #!/usr/bin/python3
 
 import os
